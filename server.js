9cf62a8ae5bb00f17367a09ff30eab819b788d85
Steve Unixson <unixson@gmail.com>
2018-05-03 09:11:21 +0300

    added mongodb and sendmail

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express')
 const serve   = require('express-static');
 const log = require('./libs/log')(module);
 const port = process.env.PORT || 8001;
+const bodyParser = require('body-parser');
+const usersave = require('./controllers/form');
 
 const app = express();
- 
-app.use(serve(__dirname + '/public'));
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json()) // parse application/json
+app.post('/api/user', usersave.createUser); 
+app.use(serve(__dirname + '/public'));
 app.listen(port, () => log.info('Express GraphQL Server Now Running On localhost:' + port));
